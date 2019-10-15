2eec4a3b99d25355a4b6c7f55c2cc4b71ec8665e
bertRC <sithalbert@ya.ru>
2019-10-15 19:13:34 +0300

    Созданы 5 объектов класса House с помощью сервиса HouseService

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -1,7 +1,50 @@
 package ru.itpark;
 
+import ru.itpark.model.House;
+import ru.itpark.service.HouseService;
+
 public class Main {
     public static void main(String[] args) {
+        HouseService service = new HouseService();
 
+        service.add(new House(
+                1,
+                "2-к квартира, 63 м², 5/24 эт.",
+                "https://06.img.avito.st/640x480/6006342406.jpg",
+                11_000_000,
+                null,
+                "Козья Слобода, 1,2 км, Республика Татарстан, Казань, Ново-Савиновский район, Меридианная улица, 2"));
+        service.add(new House(
+                2,
+                "1-к квартира, 46 м², 6/9 эт.",
+                "https://01.img.avito.st/640x480/5163598701.jpg",
+                1_000,
+                "за сутки",
+                "Козья Слобода, 3,4 км, Республика Татарстан, Казань, улица Маршала Чуйкова, 63"));
+        service.add(new House(
+                3,
+                "Комната 25 м² в 3-к, 2/2 эт.",
+                "https://83.img.avito.st/640x480/6146122183.jpg",
+                1_500,
+                "за сутки",
+                "Дубравная, 4,1 км, Республика Татарстан, Казань, жилой массив"));
+        service.add(new House(
+                4,
+                "Коттедж 150.3 м² на участке 5 сот.",
+                "https://39.img.avito.st/640x480/6131885939.jpg",
+                5_900_000,
+                null,
+                "Оренбургский тракт, 15 км, Столбище, Советская"));
+        service.add(new House(
+                5,
+                "Коттедж 175 м² на участке 15 сот.",
+                "https://24.img.avito.st/640x480/5581959624.jpg",
+                4_000,
+                "за сутки",
+                "Мамадышский тракт, Республика Татарстан, Казань, жилой массив Вознесенское, Дубравная улица, 14"));
+
+        for (House house : service.getHouses()) {
+            System.out.println(house);
+        }
     }
 }
