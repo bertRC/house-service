8507bad02dbd3a9576c1472182289b1d8fee6335
bertRC <sithalbert@ya.ru>
2019-10-15 19:04:40 +0300

    Добавлен конструктор и метод add, необходимые для работы ArrayList

--- src/main/java/ru/itpark/service/HouseService.java
+++ src/main/java/ru/itpark/service/HouseService.java
@@ -2,10 +2,15 @@ package ru.itpark.service;
 
 import ru.itpark.model.House;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HouseService {
     private List<House> houses;
+
+    public HouseService() {
+        houses = new ArrayList<>();
+    }
 
     public List<House> getHouses() {
         return houses;
@@ -13,6 +18,10 @@ public class HouseService {
 
     public void setHouses(List<House> houses) {
         this.houses = houses;
+    }
+
+    public void add(House house) {
+        houses.add(house);
     }
 }
 
