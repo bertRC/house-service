9d4c7aca8d20a08e67b11176fcf2c78d66834ccf
bertRC <sithalbert@ya.ru>
2019-10-15 20:58:55 +0300

    Сокращенный поиск - перегрузка метода search

--- src/main/java/ru/itpark/service/HouseService.java
+++ src/main/java/ru/itpark/service/HouseService.java
@@ -24,7 +24,27 @@ public class HouseService {
         houses.add(house);
     }
 
-    public List<House> search(int minPrice, int maxPrice, String address, boolean isToBuy, boolean isToRent) {
+    public List<House> search(int minPrice, int maxPrice) {
+        return this.search(minPrice, maxPrice, "", true, true);
+    }
+
+    public List<House> search(String address) {
+        return this.search(0, 0, address, true, true);
+    }
+
+    public List<House> search(int minPrice, int maxPrice, String address) {
+        return this.search(minPrice, maxPrice, address, true, true);
+    }
+
+    public List<House> searchToBuy(int minPrice, int maxPrice, String address) {
+        return this.search(minPrice, maxPrice, address, true, false);
+    }
+
+    public List<House> searchToRent(int minPrice, int maxPrice, String address) {
+        return this.search(minPrice, maxPrice, address, false, true);
+    }
+
+    private List<House> search(int minPrice, int maxPrice, String address, boolean isToBuy, boolean isToRent) {
         List<House> results = new ArrayList<>();
         for (House house : houses) {
             boolean suitableForBuyRent = isToBuy && house.getPayPeriod() == null || isToRent && house.getPayPeriod() != null;
