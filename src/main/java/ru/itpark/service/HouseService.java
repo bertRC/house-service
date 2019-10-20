a7e16363734ffa8133c7c1fae80a9f5b31e6bfa2
bertRC <sithalbert@ya.ru>
2019-10-20 20:06:09 +0300

    Небольшие исправления

--- src/main/java/ru/itpark/service/HouseService.java
+++ src/main/java/ru/itpark/service/HouseService.java
@@ -31,15 +31,9 @@ public class HouseService {
     private List<House> search(int minPrice, int maxPrice, String address) {
         List<House> results = new ArrayList<>();
         for (House house : houses) {
-//            boolean suitableForMinPrice = house.getPrice() >= minPrice;
-//            boolean suitableForMaxPrice = house.getPrice() <= maxPrice || maxPrice == 0;
-//            boolean suitableForAddress = house.getAddress().toLowerCase().contains(address.toLowerCase()) || house.getTitle().toLowerCase().contains(address.toLowerCase());
-//            if (suitableForMinPrice & suitableForMaxPrice & suitableForAddress) {
-//                results.add(house);
-//            }
-            if (house.getPrice() >= minPrice && house.getPrice() <= maxPrice && house.getAddress().toLowerCase().contains(address.toLowerCase())) {
+            if (house.getPrice() <= maxPrice && house.getPrice() >= minPrice && house.getAddress().toLowerCase().contains(address.toLowerCase())) {
                 results.add(house);
-            } else if (house.getPrice() >= minPrice && maxPrice == 0 && house.getAddress().toLowerCase().contains(address.toLowerCase())) {
+            } else if (maxPrice == 0 && house.getPrice() >= minPrice && house.getAddress().toLowerCase().contains(address.toLowerCase())) {
                 results.add(house);
             }
         }
